1b947588a403b718c223159d7a4d3198618a4a4e
davidchencodes-star <david.chen.codes@gmail.com>
2025-10-11 17:30:25 -0700

    feat(immobilien): fetch estate data from WordPress REST API

--- src/components/Total.js
+++ src/components/Total.js
@@ -6,8 +6,19 @@ import RegisterModalOne from "./Modal/RegisterModalOne";
 import RegisterModalTwo from "./Modal/RegisterModalTwo";
 import { useTotalContext } from "../context/TotalContext";
 
+import { useEffect, useState } from "@wordpress/element";
+
 const TOTAL = () => {
-    const { loading } = useTotalContext();
+    const [homeData, setHomeData] = useState([]);
+
+    useEffect(() => {
+        const pathParts = window.location.pathname.split('/').filter(Boolean);
+        const id_extern = pathParts[pathParts.length - 1];
+        fetch(`/wp-json/reactplug/v1/estate?id_extern=${id_extern}`)
+            .then(res => res.json())
+            .then(data => setHomeData(data))
+            .catch(err => console.error(err));
+    }, []);
     
     return (
         <>
